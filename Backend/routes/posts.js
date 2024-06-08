4af88a78ae96fe780d80e1867bc1b8dcf8625ea7
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-06-08 10:54:29 +0530

    Update posts.js

--- Backend/routes/posts.js
+++ Backend/routes/posts.js
@@ -26,7 +26,32 @@ router.put("/:id",async(req,res)=>{
   }
 });
 
+//Like a post
+router.put("/:id/like",async(req,res)=>{
+  try{
+    const post = await Post.findById(req.params.id);
+    if(!post.likes.includes(req.body.userId)){
+      await post.updateOne({ $push: {likes: req.body.userId}});
+      res.status(200).json("The post has been lliked");
+    }else{
+      await post.updateOne({$pull: {likes: req.body.userId}});
+      res.status(200).json("The post has been disliked");
+    }
+  } catch(err){
+    res.status(500).json(err);
+  }
+})
 
+//Get a post
+
+router.get("/:id",async(req,res)=>{
+  try{
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  }catch(err){
+    res.status(500).json(err);
+  }
+})
 
 
 module.exports = router;
