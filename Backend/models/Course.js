809effb7296c5dad8b8d507dcb6cc290ed2e7de3
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-06-18 17:32:57 +0530

    Update Course.js

--- Backend/models/Course.js
+++ Backend/models/Course.js
@@ -1,1 +1,26 @@
+const mongoose = require("mongoose");
+const CourseSchema = new mongoose.Schema({
+    userId:{
+      type:String,
+      required:true,
+    },
+  coursename:{
+    type:String,
+    required:true,
+  },
+  desc:{
+    type:String,
+    max:500,
+  },
+  img:{
+    type:String,
+  },
+  applicants:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  }]
+},
+{timestamps:true},
+);
+module.exports = mongoose.model("Course",CourseSchema);
 
