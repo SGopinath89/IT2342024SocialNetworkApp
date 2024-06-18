11af5c2fef2c9e95eacb5ca425c25c2d73b83772
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-06-18 17:46:11 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
 const postRoute = require("./routes/posts")
+const courseRoute = require("./routes/courses")
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.use(morgan("common"));
 app.use("/api/users" , userRoute);
 app.use("/api/auth" , authRoute);
 app.use("/api/posts" , postRoute);
+app.use("/api/courses" , courseRoute);
 
 app.listen(8800,()=>{
     console.log("Backend server is running");
