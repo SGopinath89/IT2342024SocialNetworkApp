2669fd0d83f23d06762b5707e29718ba6778fe70
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-06-18 07:22:41 +0530

    Update Video.js

--- Backend/models/Video.js
+++ Backend/models/Video.js
@@ -1,1 +1,38 @@
+const mongoose = require('mongoose');
+
+const videoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  videoUrl: {
+    type: String,
+    required: true,
+  },
+  uploadedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  uploadDate: {
+    type: Date,
+    default: Date.now,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Video = mongoose.model('Video', videoSchema);
+
+module.exports = Video;
 
