4978a21606fa4833fe913404d6f0f2a78b681c5d
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-07-07 21:51:28 +0530

    Update Profile.js

--- Backend/models/Profile.js
+++ Backend/models/Profile.js
@@ -1,1 +1,19 @@
 
+onst mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema({
+title: String,
+  description: String,
+  filename: String,
+  path: String,
+  size: Number,
+  uploadDate: { type: Date, default: Date.now },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  likes:{type:Number,default:0},
+  comments:{type:Array,default:[]},
+  profilePicture: { type: String, default: 'uploads/profile.png' }, 
+});
+
+const Profile = mongoose.model('Profile', profileSchema);
+module.exports = Profile;
