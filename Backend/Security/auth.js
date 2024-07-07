9481d63c7fd2ee498f6fed823a4fc33599e9ec4b
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-07-07 23:39:03 +0530

    Update auth.js

--- Backend/Security/auth.js
+++ Backend/Security/auth.js
@@ -1,1 +1,21 @@
+const secretKey = "project@vau.lk";
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+    const authHeader = req.header('Authorization');
+    if (!authHeader) return res.status(401).send({ message: 'Access Denied. No Token Provided.' });
+
+    const token = authHeader.replace('Bearer ', '');
+    if (!token) return res.status(401).send({ message: 'Access Denied. No Token Provided.' });
+
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        req.user = decoded; // Attach the decoded token to the request object
+        next();
+    } catch (ex) {
+        res.status(400).send({ message: 'Invalid Token.' });
+    }
+}
+
+module.exports = auth;
 
