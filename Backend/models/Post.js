bacb56bfa6317c8fd2c4873fd2cd9a995ea1d00f
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-07-07 23:26:59 +0530

    Update Post.js

--- Backend/models/Post.js
+++ Backend/models/Post.js
@@ -1,24 +1,32 @@
+
 const mongoose = require("mongoose")
 
-const PostSchema = new mongoose.Schema({
-  userId:{
-    type:String,
-    required:true,
-  },
-  desc:{
+const PostSchema=new mongoose.Schema({
+    userId:{
+        type:String,
+        required:true,
+        ref:"User"
+    },
+desc:{
     type:String,
     max:500,
-  },
-  img:{
+},
+img:{
     type:String
-  },
-  likes:{
-    type:String,
-    default:[],
-  },
+},
+likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+}],
+comments: [{
+    text: String,
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}],
 },
 {timestamps:true}
 );
-                                
 
 module.exports=mongoose.model("Post", PostSchema);
