c6a72ce02dd221321de3f446ef8d500c04a068e0
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-07-07 21:56:58 +0530

    Update share.js

--- Backend/models/share.js
+++ Backend/models/share.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ShareSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   itemId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  itemModel: { type: String, required: true, enum: ['Post', 'JobVacancy', 'GroupPost', 'Course','Video'] },
+  itemModel: { type: String, required: true, enum: ['Post', 'JobVacancy', 'GroupPost', 'Course','Video','Profile'] },
   sharedAt: { type: Date, default: Date.now }
 });
 
