a25c479fac0cf42200e39ae591044586ceb6d031
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-07-07 23:58:39 +0530

    Update auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -35,39 +35,7 @@ router.post("/register", async (req, res) => {
 });
 
 
-    //const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-
-    /*router.post("/login", async (req, res) => {
-      try {
-          const user = await User.findOne({ email: req.body.email });
-          if (!user) {
-              return res.status(404).json({ message: "User not found" });
-          }
-          const validPassword = await bcrypt.compare(req.body.password, user.password);
-          if (!validPassword) {
-              return res.status(400).json({ message: "Wrong password" });
-          }
-          res.status(200).json({ message: "Login successful", user });
-          //const token = jwt.sign({username:user.username},secretkey)
-          //const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-         
-      } catch (err) {
-          console.error("Error during login:", err);  // Log the error
-          res.status(500).json({ error: err.message });  // Send back an error response
-      }
-  });*/
-  /*router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) return res.status(404).send({ message: 'User not found' });
-
-    const isValidPassword = await bcrypt.compare(password, user.password);
-    if (!isValidPassword) return res.status(400).send({ message: 'Invalid password' });
-
-    const token = jwt.sign({ userId: user._id }, 'your_secret_key', { expiresIn: '10h' });
-    res.send({ token, user: { id: user._id, username: user.username, email: user.email } });
-});*/
-
+    
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   try {
