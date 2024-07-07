033b52534ac53b2518b7159e15a30eeb0b8e672c
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-07-07 23:25:14 +0530

    Update bookmark.js

--- Backend/models/bookmark.js
+++ Backend/models/bookmark.js
@@ -1,1 +1,26 @@
+const mongoose = require('mongoose');
+
+const BookmarkSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  itemId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    //refPath: 'itemModel',
+  },
+  itemModel: {
+    type: String,
+    required: true,
+    enum: ['Post', 'JobVacancy', 'GroupPost','Video','Course','Event','Share','Profile'],  // Add any other models you have
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Bookmark', BookmarkSchema);
 
