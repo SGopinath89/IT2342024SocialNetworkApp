dcab51b4ad53f3f72625dd21b6a3838b447ce305
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-07-07 23:15:42 +0530

    Update upload.js

--- Backend/config/upload.js
+++ Backend/config/upload.js
@@ -1,1 +1,32 @@
+const multer = require('multer');
+const path = require('path');
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 100000000 }, // 100MB limit
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single('file');
+
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|gif|mp4|mkv|mov/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Videos Only!');
+  }
+}
+
+module.exports = upload;
