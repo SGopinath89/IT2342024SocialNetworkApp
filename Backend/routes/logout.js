f8ac5ed459475604d47bec63f8fb629861ac0e1b
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-07-07 23:27:17 +0530

    Update logout.js

--- Backend/routes/logout.js
+++ Backend/routes/logout.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+const {verifyToken} = require('../Security/autho');
+
+router.post('/logout', verifyToken, (req, res) => {
+    res.status(200).json({ message: 'Successfully logged out' });
+});
+
+module.exports = router;
 
