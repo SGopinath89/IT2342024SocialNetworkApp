60e4b4efa49ca1616c413ebad0a1c6ca5a859fa0
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-07-07 23:24:30 +0530

    Update share.js

--- Backend/models/share.js
+++ Backend/models/share.js
@@ -3,8 +3,15 @@ const mongoose = require('mongoose');
 const ShareSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   itemId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  itemModel: { type: String, required: true, enum: ['Post', 'JobVacancy', 'GroupPost', 'Course','Video','Profile'] },
-  sharedAt: { type: Date, default: Date.now }
+  itemModel: { type: String, required: true, enum: ['Post','Event', 'JobVacancy', 'GroupPost', 'Course','Video','Profile'] },
+  sharedAt: { type: Date, default: Date.now },
+  comments: [
+    {
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+      comment: { type: String, required: true },
+      commentedAt: { type: Date, default: Date.now }
+    }
+  ]
 });
 
 module.exports = mongoose.model('Share', ShareSchema);
