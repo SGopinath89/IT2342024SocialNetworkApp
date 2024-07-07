2d556100c5f1db3df7db78d5b00ae04b515cdb91
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-07-07 23:26:27 +0530

    Update Event.js

--- Backend/models/Event.js
+++ Backend/models/Event.js
@@ -1,22 +1,37 @@
 const mongoose = require("mongoose");
 
-const EventSchema=new mongoose.Schema({
-    userId:{
-        type:String,
-        required:true,
+const ParticipantSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    eventname:{
-        type:String,
-        required:true,
+    idNumber: {
+        type: String,
+        required: true
     },
-    desc:{
-        type:String,
-        max:500,
+    contactNumber: {
+        type: String,
+        required: true
+    }
+});
+
+const EventSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
     },
-    img:{
-        type:String,
+    eventname: {
+        type: String,
+        required: true,
     },
-},
-{timestamps:true},
-);
-module.exports=mongoose.model("Event",EventSchema);
+    desc: {
+        type: String,
+        max: 500,
+    },
+    img: {
+        type: String,
+    },
+    participants: [ParticipantSchema] // Array of participants
+}, { timestamps: true });
+
+module.exports = mongoose.model("Event", EventSchema);
